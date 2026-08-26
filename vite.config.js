15365129adb83651f22a1d3bcf7f11060bceadef
Sarath <sarathgnair21@gmail.com>
2026-08-26 18:44:43 +0530

    Update Tailwind Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -21,7 +22,7 @@ export default defineConfig(({ mode }) => {
   };
 
   return {
-    plugins: [react()],
+    plugins: [tailwindcss(), react()],
     server: {
       // Serve index.html for all routes so React Router handles them client-side.
       historyApiFallback: true,
